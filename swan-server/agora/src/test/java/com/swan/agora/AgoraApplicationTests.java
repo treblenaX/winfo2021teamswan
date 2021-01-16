0ba003d344e3d324fa629687f235664447322825
Elbert Cheng <elbertkcheng@outlook.com>
2021-01-15 23:27:37 -0800

    Backend init - ignore Person stuff

---
+++ swan-server/agora/src/test/java/com/swan/agora/AgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swan.agora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
